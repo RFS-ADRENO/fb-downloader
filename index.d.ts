fc9ad1a901de5a4712b949f29e515faa8fbff988
RFS-ADRENO <dungto76@gmail.com>
2023-06-09 00:44:52 +0700

    added definition

---
+++ index.d.ts
@@ -0,0 +1,19 @@
+/**
+ *
+ * @param {string} videoUrl - Facebook video URL (required)
+ * @param {string} [cookie] - Facebook cookie (optional)
+ * @param {string} [useragent] - User agent (optional)
+ */
+export declare function getVideoInfo(
+    videoUrl: string,
+    cookie?: string,
+    useragent?: string
+): Promise<Video>;
+
+export interface VideoInfo {
+    url: string;
+    sd: string;
+    hd: string;
+    title: string;
+    thumbnail: string;
+}
